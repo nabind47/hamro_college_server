4f4dba3b0e3f3575927dd9a59ee51aa33bd363a0
NabinDhami201347 <nabin.201347@ncit.edu.np>
2023-07-03 06:25:54 +0545

    feat: upload files and images

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,5 +1,125 @@
-import multer from 'multer';
+import { Request, Response, NextFunction } from 'express';
+import multer, { MulterError } from 'multer';
+import path from 'path';
+import fs from 'fs';
 
-const upload = multer({ dest: 'uploads/' });
+// Set up multer storage for files
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const destinationFolder = 'public/files';
+    createFolderIfNotExists(destinationFolder);
+    cb(null, destinationFolder);
+  },
+  filename: (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9) + fileExtension;
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
 
-export default upload;
+// Set up multer storage for images
+const imageStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const destinationFolder = 'public/images';
+    createFolderIfNotExists(destinationFolder);
+    cb(null, destinationFolder);
+  },
+  filename: (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9) + fileExtension;
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
+
+// File size limit in bytes (e.g., 5MB)
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+
+// Allowed mime types for files
+const FILE_ALLOWED_MIME_TYPES = [
+  'application/pdf',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  'application/vnd.ms-excel',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  'application/vnd.ms-powerpoint',
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+  'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+  'application/vnd.ms-powerpoint.template.macroEnabled.12',
+  'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+  'application/vnd.ms-powerpoint.slide.macroEnabled.12',
+  'application/vnd.ms-powerpoint.show.macroEnabled.12',
+];
+
+// Allowed mime types for images
+const IMAGE_ALLOWED_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif'];
+
+// Create multer instances for files and images
+const fileUpload = multer({
+  storage: fileStorage,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
+  fileFilter: (req, file, cb) => {
+    if (FILE_ALLOWED_MIME_TYPES.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(
+        new Error(
+          'Invalid file type. Only documents, PDFs, Excel, and Power BI files are allowed.',
+        ),
+      );
+    }
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
+  fileFilter: (req, file, cb) => {
+    if (IMAGE_ALLOWED_MIME_TYPES.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid image type. Only JPEG, PNG, and GIF images are allowed.'));
+    }
+  },
+});
+
+// Create destination folder if it doesn't exist
+const createFolderIfNotExists = (folderPath: string) => {
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+};
+
+// Middleware function for uploading a single file
+export const uploadFileMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  fileUpload.single('file')(req, res, (err: any) => {
+    if (err) {
+      if (err instanceof MulterError) {
+        if (err.code === 'LIMIT_FILE_SIZE') {
+          return res.status(400).json({ error: 'File size is too large' });
+        }
+      }
+      return res.status(400).json({ error: err.message });
+    }
+    next();
+  });
+};
+
+// Middleware function for uploading a single image
+export const uploadImageMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  imageUpload.single('image')(req, res, (err: any) => {
+    if (err) {
+      if (err instanceof MulterError) {
+        if (err.code === 'LIMIT_FILE_SIZE') {
+          return res.status(400).json({ error: 'File size is too large' });
+        }
+      }
+      return res.status(400).json({ error: err.message });
+    }
+    next();
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,38 @@
 import express from 'express';
 import cors from 'cors';
-// import cloudinary from 'cloudinary';
-// import { unlinkSync } from 'fs';
-// import upload from './middlewares/upload.middleware';
+import path from 'path';
 
 import authRoutes from './modules/auth/auth.routes';
 import noticeRoutes from './modules/notice/notice.routes';
+import { uploadFileMiddleware, uploadImageMiddleware } from './middlewares/upload.middleware';
 
 const app = express();
 app.use(express.json());
+
+// Set up the static file middleware to serve the uploaded files
+app.use('/public/files', express.static(path.join(__dirname, '..', 'public', 'files')));
+app.use('/public/images', express.static(path.join(__dirname, '..', 'public', 'images')));
+
 // Apply CORS middleware
 app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', noticeRoutes);
 
-// Configure Cloudinary credentials
-// cloudinary.v2.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'djq7boegg',
-//   api_key: process.env.CLOUDINARY_API_KEY || '834811133725895',
-//   api_secret: process.env.CLOUDINARY_API_SECRET || 'J04uJ4CU3h86ZbBENHi1yiSzLwg',
-// });
+// http://localhost:1337/public/images/image-1688344250817-327880280.png
+app.post('/image', uploadImageMiddleware, (req, res) => {
+  // Access the uploaded image using 'req.file'
+  // Do something with the image, e.g., save it to a database, process it, etc.
+  console.log(req.file);
+  res.json({ message: 'File uploaded successfully!' });
+});
 
-// app.post('/upload', upload.single('image'), async (req: Request, res: Response) => {
-//   try {
-//     const { file } = req;
-
-//     if (!file) {
-//       return res.status(400).json({ error: 'No file uploaded' });
-//     }
-
-//     const { secure_url } = await cloudinary.v2.uploader.upload(file.path, {
-//       folder: 'notices', // Optional: Specify a folder in Cloudinary to store the uploaded images
-//       resource_type: 'auto', // Optional: Detect the resource type automatically (image, video, raw)
-//     });
-
-//     res.json({ url: secure_url });
-//   } catch (error) {
-//     console.error('Error uploading image:', error);
-//     res.status(500).json({ error: 'Failed to upload image' });
-//   } finally {
-//     if (req.file) {
-//       unlinkSync(req.file.path); // Remove the temporary file
-//     }
-//   }
-// });
-
-app.get('/', (req, res) => {
-  res.send('Hello');
+// http://localhost:1337/public/files/file-1688344296566-872167895.pdf
+app.post('/file', uploadFileMiddleware, (req, res) => {
+  // Access the uploaded image using 'req.file'
+  // Do something with the image, e.g., save it to a database, process it, etc.
+  console.log(req.file);
+  res.json({ message: 'File uploaded successfully!' });
 });
 
 app.listen(1337, () => {
